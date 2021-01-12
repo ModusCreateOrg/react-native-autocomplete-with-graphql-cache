0a2cca8ff57e77aa9b57cecd8ee082109120841f
Braulio Barrera <barrerabraulio17@gmail.com>
2021-01-12 12:40:21 -0600

    change folder structure and modify readme

--- modus-autocomplete/ModusAutocomplete/src/components/Search.js
+++ modus-autocomplete/ModusAutocomplete/src/components/Search.js
@@ -92,13 +92,6 @@ const Search: (props) => React$Node = (props) => {
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
   body: {
     backgroundColor: Colors.white,
     zIndex: 10,
@@ -115,23 +108,6 @@ const styles = StyleSheet.create({
     fontWeight: '600',
     color: Colors.black,
     margin: 10,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
   },
   text: {
     fontSize: 11,
